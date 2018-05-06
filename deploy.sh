cfbcc34dd8cd07d3ead74a06258661b869cd73eb
Paolo Escobar <paolo.escobar@hubesco.com>
2018-05-06 12:30:21 +0100

    Test TravisCI branch

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "development" ]; then
+  // do the deploy
+  eval "$(ssh-agent -s)" #start the ssh agent
+  chmod 600 .travis/deploy.key # this key should have push access
+  ssh-add .travis/deploy.key
+  ssh-keyscan ns377672.ip-37-59-60.eu >> ~/.ssh/known_hosts
+  git remote add deploy dokku@ns377672.ip-37-59-60.eu:famivac-gestionnaire-app-int
+  git config --global push.default simple
+  git push deploy development:master
+fi
