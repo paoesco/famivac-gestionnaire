cbea96549dff42513050019b341cd95863ba91fc
Paolo Escobar <paolo.escobar@hubesco.com>
2019-01-31 22:47:41 +0000

    Adds deployment to prod when push on master

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,13 @@ if [ "$TRAVIS_BRANCH" == "development" ]; then
   git config --global push.default simple
   git push deploy development:master
 fi
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  eval "$(ssh-agent -s)" #start the ssh agent
+  chmod 600 .travis/deploy.key # this key should have push access
+  ssh-add .travis/deploy.key
+  ssh-keyscan ns377672.ip-37-59-60.eu >> ~/.ssh/known_hosts
+  git remote add deploy dokku@ns377672.ip-37-59-60.eu:famivac-gestionnaire-app-prod
+  git config --global push.default simple
+  git push deploy master:master
+fi
