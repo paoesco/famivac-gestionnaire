fcbf7603fe4eba79036f1d50258db43e30a46811
Paolo Escobar <paolo.escobar@hubesco.com>
2019-02-01 14:46:43 +0000

    Prepares for SaaS model

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "development" ]; then
-  eval "$(ssh-agent -s)" #start the ssh agent
-  chmod 600 .travis/deploy.key # this key should have push access
-  ssh-add .travis/deploy.key
-  ssh-keyscan ns377672.ip-37-59-60.eu >> ~/.ssh/known_hosts
-  git remote add deploy dokku@ns377672.ip-37-59-60.eu:famivac-gestionnaire-app-int
-  git config --global push.default simple
-  git push deploy development:master
-fi
-
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   eval "$(ssh-agent -s)" #start the ssh agent
   chmod 600 .travis/deploy.key # this key should have push access
