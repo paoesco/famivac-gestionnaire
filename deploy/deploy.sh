0c9ccf8a3c5299ea7b7db59ebd37e64f7bfe0aa7
Paolo Escobar <paolo.escobar@hubesco.com>
2019-09-06 10:09:46 +0200

    Deploys only on master

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  eval "$(ssh-agent -s)" #start the ssh agent
-  chmod 600 deploy/deploy.key # this key should have push access
-  ssh-add deploy/deploy.key
-  ssh-keyscan ns377672.ip-37-59-60.eu >> ~/.ssh/known_hosts
-  git remote add deploy dokku@ns377672.ip-37-59-60.eu:famivac-gestionnaire-app-prod
-  git config --global push.default simple
-  git push deploy master:master
-fi
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 deploy/deploy.key # this key should have push access
+ssh-add deploy/deploy.key
+ssh-keyscan ns377672.ip-37-59-60.eu >> ~/.ssh/known_hosts
+git remote add deploy dokku@ns377672.ip-37-59-60.eu:famivac-gestionnaire-app-prod
+git config --global push.default simple
+git push deploy master:master
