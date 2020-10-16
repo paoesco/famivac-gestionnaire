237b9600e5166ec345bdb800262aef08c732c14f
Paolo Escobar <paolo@solidstategroup.com>
2020-10-16 12:45:03 +0100

    Removes email

--- README.md
+++ README.md
@@ -14,13 +14,7 @@
 
 ## Startup
 
-Environment properties :
-
-- swarm.mail.smtp.host
-- swarm.mail.smtp.port
-- swarm.mail.mail-sessions.famivac.smtp-server.username
-- swarm.mail.mail-sessions.famivac.smtp-server.password
-- swarm.mail.mail-sessions.famivac.smtp-server.ssl
+System properties :
 - swarm.ds.name
 - swarm.ds.connection.url
 - swarm.ds.username
@@ -36,11 +30,6 @@ java
     -Dswarm.ds.username=$JDBC_DATABASE_USERNAME 
     -Dswarm.ds.password=$JDBC_DATABASE_PASSWORD 
     -Djavamelody.datasources=java:jboss/datasources/PostgreSQLDS 
-    -Dswarm.mail.smtp.host=$MAIL_SERVER_SMTP_HOST 
-    -Dswarm.mail.smtp.port=$MAIL_SERVER_SMTP_PORT 
-    -Dswarm.mail.mail-sessions.famivac.smtp-server.username=$MAIL_SERVER_SMTP_USERNAME 
-    -Dswarm.mail.mail-sessions.famivac.smtp-server.password=$MAIL_SERVER_SMTP_PASSWORD 
-    -Dswarm.mail.mail-sessions.famivac.smtp-server.ssl=true
     -Dswarm.http.port=$PORT 
     -jar gestionnaire-web/target/gestionnaire-web-*-swarm.jar
 
--- gestionnaire-web/src/main/java/fr/famivac/gestionnaire/interfaces/facade/utilisateurs/UtilisateurServiceFacade.java
+++ gestionnaire-web/src/main/java/fr/famivac/gestionnaire/interfaces/facade/utilisateurs/UtilisateurServiceFacade.java
@@ -2,9 +2,6 @@ package fr.famivac.gestionnaire.interfaces.facade.utilisateurs;
 
 import fr.famivac.gestionnaire.administration.authentication.control.AjouterUtilisateurDTO;
 import fr.famivac.gestionnaire.administration.authentication.control.UtilisateurService;
-import fr.famivac.gestionnaire.email.control.Mail;
-import fr.famivac.gestionnaire.email.control.MailService;
-import java.text.MessageFormat;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -18,26 +15,14 @@ public class UtilisateurServiceFacade {
     @Inject
     private UtilisateurService utilisateurService;
 
-    @Inject
-    private MailService mailService;
 
     public String create(AjouterUtilisateurDTO dto) {
         final String password = utilisateurService.create(dto);
-        Mail mail = new Mail(
-                dto.getEmail(),
-                "[FAMIVAC] Création d'un compte",
-                MessageFormat.format("Un administrateur vous a créé un compte sur l''interface https://gestionnaire.famivac.fr<br/> Login : {0} \n Mot de passe généré : {1} \n Veuillez le changer à la première connexion.", dto.getLogin(), password));
-        mailService.send(mail);
         return password;
     }
 
     public String reinitPassword(String login, String email) {
         String password = utilisateurService.reinitPassword(login);
-        Mail mail = new Mail(
-                email,
-                "[FAMIVAC] Réinitialisation mot de passe",
-                MessageFormat.format("Un administrateur a réinitialisé votre mot de passe sur le site https://gestionnaire.famivac.fr<br/> Nouveau mot de passe : {0}", password));
-        mailService.send(mail);
         return password;
     }
 
